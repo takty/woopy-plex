6c9e25feb9964d88d207c46d166d7c29dd0f8ad1
Takuto Yanagida <yanagida@stxst.com>
2021-03-10 17:19:28 +0900

    Add files

---
+++ src/post-content.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Post Content
+ *
+ * @package Wpinc Plex
+ * @author Takuto Yanagida
+ * @version 2021-03-10
+ */
+
+namespace wpinc\plex\post_content;
+
+require_once __DIR__ . '/custom-rewrite.php';
+
+
---
+++ src/taxonomy.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Taxonomy
+ *
+ * @package Wpinc Plex
+ * @author Takuto Yanagida
+ * @version 2021-03-10
+ */
+
+namespace wpinc\plex\taxonomy;
+
+require_once __DIR__ . '/custom-rewrite.php';
+
+
